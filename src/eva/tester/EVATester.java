9efcdd85052cde42c5bb5e341f53e33a35f9753f
Johaned <johan.tique@codescrum.com>
2014-04-26 23:54:56 -0500

    add support to async and parallel processes

--- src/eva/tester/EVATester.java
+++ src/eva/tester/EVATester.java
@@ -6,9 +6,11 @@ package eva.tester;
 
 import com.google.gson.Gson;
 import eva.tester.config.AppConfig;
+import eva.tester.model.Report;
 import eva.tester.processor.Tester;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import org.university.stcav.eva.model.AudioElement;
 import org.university.stcav.eva.model.MediaElement;
@@ -24,15 +26,30 @@ public class EVATester {
     /**
      * @param args the command line arguments
      */
+    public static Report report;
     
     public static void main(String[] args) throws InterruptedException, IOException {
         AppConfig appConfig = AppConfig.getInstance();
         MediaElement me = Processor.get_mediaElement("16_9.mp4", System.getProperty("user.dir")+"/"+appConfig.getProperty("media_path"), true);
         MediaElement meConfig = new MediaElement(new AudioElement("libfdk_aac", "48000", "stereo", "224k"), new VideoElement("libx264", "1280x720", "16:9", "2000k", "25"), System.getProperty("user.dir")+"/"+appConfig.getProperty("output_path")+ "/");
-        System.out.println("-->"+ me.getName());
+        
+        MediaElement me2 = Processor.get_mediaElement("16_9.mp4", System.getProperty("user.dir")+"/"+appConfig.getProperty("media_path"), true);
+        MediaElement meConfig2 = new MediaElement(new AudioElement("libfdk_aac", "48000", "stereo", "224k"), new VideoElement("libx264", "1280x720", "16:9", "2000k", "25"), System.getProperty("user.dir")+"/"+appConfig.getProperty("output_path")+ "/");
+        
+        // creating report
+        report = new Report();
+        report.setDate(new Date(System.currentTimeMillis()));
+        
         List actions = new ArrayList();
         actions.add(Tester.RESIZE_VIDEO_HD);
+        
         Tester tester = new Tester(me, meConfig, actions);
-        tester.run();
+        Thread.sleep(10);
+        Tester tester2 = new Tester(me2, meConfig2, actions);
+        tester.execute();
+        tester2.execute();
+        
+        Gson gson = new Gson();
+        System.err.println("output: "+gson.toJson(report));
     }
 }
--- src/eva/tester/processor/Tester.java
+++ src/eva/tester/processor/Tester.java
@@ -4,10 +4,14 @@
  */
 package eva.tester.processor;
 
+import eva.tester.EVATester;
 import eva.tester.config.AppConfig;
 import eva.tester.conn.RestClient;
+import eva.tester.model.Action;
+import eva.tester.model.MediaProcess;
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.logging.Level;
@@ -30,16 +34,21 @@ public class Tester implements Runnable{
     private String id;
     // Action List
     private List<Integer> actions;
+    // Perfomed Action List
+    private List<Action> performedActions;
     // execute from local
     private Boolean executeFromLocal = false;
     // communication interface
     private RestClient rc;
+    // thread pointer
+    private Thread process;
 
     public Tester(MediaElement me, MediaElement meConfig, List actions) throws UnknownHostException, InterruptedException, IOException {
         this.me = me;
         this.meConfig = meConfig;
         this.actions = actions;
         this.id = String.valueOf(System.currentTimeMillis());
+        this.performedActions = new ArrayList<>();
         this.rc = new RestClient();
         buildTestScenary();    
     }
@@ -99,19 +108,36 @@ public class Tester implements Runnable{
         }
         Iterator iterator = actions.iterator();
         String command;
+        Action action;
+        int actionId;
+        long startTime, endTime;
         while(iterator.hasNext()){
+           actionId = (Integer) iterator.next();
+           action = new Action();
+           action.setId(actionId);
             try {
-                command = getCommand((Integer) iterator.next());
+                command = getCommand(actionId);
+                action.setStartTime(System.currentTimeMillis());
                 if(!executeFromLocal){
                     String response = rc.get(appConfig.getProperty("process_executor_server_url") + command.replace(" ", "¬"));
                     System.out.println("[LOG] from <"+id+"> :"+response);
                 }else{
                     ProcessExecutor.execute_process(command.split(" "), "/", true);
                 }
+                action.setEndTime(System.currentTimeMillis());
+                action.setSuccess(true);
             } catch (    InterruptedException | IOException ex) {
+                action.setSuccess(false);
                 Logger.getLogger(Tester.class.getName()).log(Level.SEVERE, null, ex);
             }
+            performedActions.add(action);
         }
+        MediaProcess mp = new MediaProcess();
+        mp.setId(this.id);
+        mp.setMe(this.me);
+        mp.setMeConfig(this.meConfig);
+        mp.setActions(performedActions);
+        EVATester.report.addMediaProcess(mp);
     }
     
     private void buildTestScenary() throws UnknownHostException, InterruptedException, IOException{
@@ -127,6 +153,11 @@ public class Tester implements Runnable{
         
         this.me.setName(meConfig.getName()+this.id+"/"+ this.me.getName());
         this.meConfig.setName(meConfig.getName()+this.id+"/"+this.id+".mp4");
+    }
+
+    public void execute() {
+       process = new Thread(this);
+       process.start();
     }
     
 }
