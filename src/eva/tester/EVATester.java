e2f7e9a79293e05a4e2591ec2669fda62b0529fd
Johaned <johan.tique@codescrum.com>
2014-04-26 21:10:53 -0500

    add first flow in order to test evatester main class

--- src/eva/tester/EVATester.java
+++ src/eva/tester/EVATester.java
@@ -6,8 +6,13 @@ package eva.tester;
 
 import com.google.gson.Gson;
 import eva.tester.config.AppConfig;
+import eva.tester.processor.Tester;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.university.stcav.eva.model.AudioElement;
 import org.university.stcav.eva.model.MediaElement;
+import org.university.stcav.eva.model.VideoElement;
 import org.university.stcav.eva.processor.Processor;
 
 /**
@@ -22,7 +27,13 @@ public class EVATester {
     
     public static void main(String[] args) throws InterruptedException, IOException {
         AppConfig appConfig = AppConfig.getInstance();
-        MediaElement me = Processor.get_mediaElement(appConfig.getProperty("video_name"), System.getProperty("user.dir")+"/"+appConfig.getProperty("media_path"), true);
-        
+        MediaElement me = Processor.get_mediaElement("16_9.avi", System.getProperty("user.dir")+"/"+appConfig.getProperty("media_path"), true);
+        MediaElement meConfig = new MediaElement(new AudioElement("libfdk_aac", "48000", "stereo", "224k"), new VideoElement("libx264", "1280x720", "16:9", "2000k", "25"), System.getProperty("user.dir")+"/"+appConfig.getProperty("output_path")+ "/");
+        System.out.println("-->"+ me.getName());
+        List actions = new ArrayList();
+        actions.add(Tester.RESIZE_VIDEO_HD);
+        Tester tester = new Tester(me, meConfig, actions);
+        tester.run();
+        //tester.execute(0);
     }
 }
