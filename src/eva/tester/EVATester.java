1fb4eb7dabae4a778fb125248da44fd19afa7969
Johaned <johan.tique@codescrum.com>
2014-04-27 19:03:20 -0500

    add support to sync all tester thread

--- src/eva/tester/EVATester.java
+++ src/eva/tester/EVATester.java
@@ -11,6 +11,7 @@ import eva.tester.processor.Tester;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import org.university.stcav.eva.model.AudioElement;
 import org.university.stcav.eva.model.MediaElement;
@@ -30,25 +31,33 @@ public class EVATester {
     
     public static void main(String[] args) throws InterruptedException, IOException {
         AppConfig appConfig = AppConfig.getInstance();
-        MediaElement me = Processor.get_mediaElement("16_9.mp4", System.getProperty("user.dir")+"/"+appConfig.getProperty("media_path"), true);
+        MediaElement me_4_3 = Processor.get_mediaElement("4_3.avi", System.getProperty("user.dir")+"/"+appConfig.getProperty("media_path"), true);
+        MediaElement me_16_9 = Processor.get_mediaElement("16_9.mp4", System.getProperty("user.dir")+"/"+appConfig.getProperty("media_path"), true);
         MediaElement meConfig = new MediaElement(new AudioElement("libfdk_aac", "48000", "stereo", "224k"), new VideoElement("libx264", "1280x720", "16:9", "2000k", "25"), System.getProperty("user.dir")+"/"+appConfig.getProperty("output_path")+ "/");
-        
-        MediaElement me2 = Processor.get_mediaElement("16_9.mp4", System.getProperty("user.dir")+"/"+appConfig.getProperty("media_path"), true);
-        MediaElement meConfig2 = new MediaElement(new AudioElement("libfdk_aac", "48000", "stereo", "224k"), new VideoElement("libx264", "1280x720", "16:9", "2000k", "25"), System.getProperty("user.dir")+"/"+appConfig.getProperty("output_path")+ "/");
         
         // creating report
         report = new Report();
         report.setDate(new Date(System.currentTimeMillis()));
         
         List actions = new ArrayList();
-        actions.add(Tester.RESIZE_VIDEO_HD);
+        actions.add(1);
         
-        Tester tester = new Tester(me, meConfig, actions);
-        Thread.sleep(10);
-        Tester tester2 = new Tester(me2, meConfig2, actions);
-        tester.execute();
-        tester2.execute();
+        List<Tester> threadMaster = new ArrayList<Tester>();
+        Iterator iter;
+        threadMaster.add(new Tester(me_4_3, meConfig, actions));
         
+        iter = threadMaster.iterator();
+        
+        while(iter.hasNext()){
+            ((Tester)iter.next()).execute();
+        }
+        
+        // it waits that all threads have finished
+        iter = threadMaster.iterator();
+        while(iter.hasNext()){
+            ((Tester)iter.next()).join();
+        }
+             
         Gson gson = new Gson();
         System.err.println("output: "+gson.toJson(report));
     }
